38f4f8072e9e1a1dd61c4c742c87a6cd1e9d9e03
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-13 21:37:04 +0700

    init sidekiq

--- api/config/application.rb
+++ api/config/application.rb
@@ -26,5 +26,7 @@ module Api
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.use ::ApiExceptionHandlerMiddleware
+    config.active_job.queue_adapter = :sidekiq
+
   end
 end
