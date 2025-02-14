94619d019ec00434de44524316b43f0740373973
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-14 21:37:18 +0700

    add test serializers

--- api/app/channels/notification_channel.rb
+++ api/app/channels/notification_channel.rb
@@ -1,13 +1,5 @@
 class NotificationChannel < ActionCable::Channel::Base
   def subscribed
     stream_from "notifications"
-    broadcast_to(
-      "notifications",
-      "hello"
-    )
-  end
-
-  def unsubscribed
-
   end
 end
