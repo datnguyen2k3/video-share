04a71b4b511771d990746cd57f7faaf07da5d18f
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-16 17:15:45 +0700

    Update handle notification

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "@/app/components/Headers";
 import styles from "@/app/styles/video.module.css";
 import axios from "axios";
@@ -8,7 +8,7 @@ import { VideoType } from "@/app/types/modals";
 import { getAuthorization } from "../../../utils/auth";
 import useAuth from "@/app/hooks/useAuth";
 import Toast from "@/app/components/Toast";
-import { useWebSocket } from "../contexts/WebSocketContext";
+import { createCable } from "@anycable/web";
 
 const VideoList: React.FC = () => {
   const [videos, setVideos] = useState<VideoType[]>([]);
@@ -17,8 +17,10 @@ const VideoList: React.FC = () => {
     message: "",
     type: "success" as "success" | "error",
   });
+  const [newMessage, setNewMessage] = useState<any>({});
+  const cableRef = useRef<any>(null);
   useAuth();
-  const { newMessage } = useWebSocket();
+
   useEffect(() => {
     if (!localStorage.getItem("userData")) {
       return;
@@ -45,11 +47,44 @@ const VideoList: React.FC = () => {
       });
   }, []);
 
-  // Add new video when receiving WebSocket message
   useEffect(() => {
-    console.log({ newMessage });
+    const userDataString = localStorage.getItem("userData");
+    if (!userDataString) {
+      return;
+    }
+    const userData: any = JSON.parse(userDataString);
+    if (!userData) {
+      return;
+    }
+    const token = userData?.auth?.access_token;
+    const cable = createCable(
+      process.env.NEXT_PUBLIC_WEBSOCKET_URL + `?token=${token}` || ""
+    );
+    cableRef.current = cable;
+    cable.connect();
+    const channel = cable.subscribeTo("NotificationChannel");
+    channel.on("message", (data: any) => {
+      setNewMessage(data);
+      console.log({ userData });
+      if (userData?.email === data.owner_email) {
+        return;
+      }
+      console.log({ data });
+      setToast({
+        show: true,
+        message: `Received message from ${data.owner_email}`,
+        type: "success",
+      });
+      return () => {
+        if (cableRef.current) {
+          cableRef.current.disconnect();
+        }
+      };
+    });
+  }, []);
+
+  useEffect(() => {
     if (newMessage && Object.keys(newMessage).length > 0) {
-      // Check if video already exists to avoid duplicates
       const videoExists = videos.some(
         (video) => video.youtube_id === newMessage.youtube_id
       );
--- frontend/src/app/shareVideo/page.tsx
+++ frontend/src/app/shareVideo/page.tsx
@@ -1,15 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "@/app/styles/youtubeShareForm.module.css";
 import Headers from "@/app/components/Headers";
 import axios from "axios";
 import { getAuthorization } from "../../../utils/auth";
 import useAuth from "@/app/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import { createCable } from "@anycable/web";
+import Toast from "../components/Toast";
 
 const YoutubeShareForm: React.FC = () => {
   const [url, setUrl] = useState<string>("");
   const router = useRouter();
+  const [toast, setToast] = useState({
+    show: false,
+    message: "",
+    type: "success" as "success" | "error",
+  });
+  const cableRef = useRef<any>(null);
   useAuth();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -26,9 +34,48 @@ const YoutubeShareForm: React.FC = () => {
     router.push("/");
   };
 
+  useEffect(() => {
+    const userDataString = localStorage.getItem("userData");
+    if (!userDataString) {
+      return;
+    }
+    const userData: any = JSON.parse(userDataString);
+    if (!userData) {
+      return;
+    }
+    const token = userData?.auth?.access_token;
+    const cable = createCable(
+      process.env.NEXT_PUBLIC_WEBSOCKET_URL + `?token=${token}` || ""
+    );
+    cableRef.current = cable;
+    cable.connect();
+    const channel = cable.subscribeTo("NotificationChannel");
+    channel.on("message", (data: any) => {
+      if (userData?.email == data.owner_email) {
+        return;
+      }
+      setToast({
+        show: true,
+        message: `Received message from ${data.owner_email}`,
+        type: "success",
+      });
+      return () => {
+        if (cableRef.current) {
+          cableRef.current.disconnect();
+        }
+      };
+    });
+  }, []);
+
   return (
     <>
       <Headers />
+      <Toast
+        show={toast.show}
+        message={toast.message}
+        type={toast.type}
+        onClose={() => setToast({ ...toast, show: false })}
+      />
       <div className={styles.container}>
         <form onSubmit={handleSubmit} className={styles.form}>
           <h2 className={styles.title}>Share a Youtube movie</h2>
