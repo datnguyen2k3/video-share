37ca0bc97107f6a97e52cce572b2ac11dc089551
datnguyen2k3 <thanhdatmu2003@gmail.com>
2025-02-16 16:22:58 +0700

    update cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
     # todo: change this to the domain of your frontend
-      origins 'https://video-share-lilac.vercel.app'  # Allow requests from your React frontend
+      origins 'http://13.212.217.117:3001'  # Allow requests from your React frontend
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
